bd1bbd604e79a3d13c7a1d6e82c646d963c1898c
MuradAles <muradaleskerov99@gmail.com>
2023-06-20 14:49:58 -0400

    2.1 solved

--- Coding_Interview/Linked_lists/2.1.py
+++ Coding_Interview/Linked_lists/2.1.py
@@ -20,16 +20,31 @@ class LinkedList:
         new_node.next = self.head
         self.head = new_node
 
-    def delete_duplicates(self):
+    def remove_duplicates(self):
         if not self.head:
-            current = self.head
+            return
+        current = self.head
+        uniq_array = []
+        while current.next:
+            if current.next.data in uniq_array:
+                current.next = current.next.next
+            else:
+                uniq_array.append(current.next.data)
+                current = current.next
 
 
 def main():
     node1 = LinkedList()
-    node1.insert_at_beginning(3)
-    node1.insert_at_beginning(2)
     node1.insert_at_beginning(1)
+    node1.insert_at_beginning(1)
+    node1.insert_at_beginning(1)
+    node1.insert_at_beginning(2)
+    node1.insert_at_beginning(2)
+    node1.insert_at_beginning(3)
+    node1.insert_at_beginning(1)
+    node1.insert_at_beginning(5)
+    node1.insert_at_beginning(6)
+    node1.remove_duplicates()
     node1.display_list()
 
 
