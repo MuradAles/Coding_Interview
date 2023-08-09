c150d08f4550023f289d695fdcd1855c89895f38
MuradAles <muradaleskerov99@gmail.com>
2023-08-09 11:52:14 -0400

    2.6 been added

--- Coding_Interview/Linked_lists/2.6.py
+++ Coding_Interview/Linked_lists/2.6.py
@@ -25,9 +25,15 @@ class LinkedList:
     def palindrome_check(self) -> bool:
         if not self.head:
             return True
-        is_palindrome = True
-
-        return is_palindrome
+        # we can create 2 lists and compare them
+        list_1 = []
+        list_2 = []
+        current = self.head
+        while current:
+            list_1.insert(0, current.data)
+            list_2.append(current.data)
+            current = current.next
+        return list_1 == list_2
 
 
 def main():
@@ -35,6 +41,9 @@ def main():
     node1.insert_at_beginning(1)
     node1.insert_at_beginning(2)
     node1.insert_at_beginning(3)
+    node1.insert_at_beginning(4)
+    node1.insert_at_beginning(2)
+    node1.insert_at_beginning(1)
     node1.display()
     print(node1.palindrome_check())
 
