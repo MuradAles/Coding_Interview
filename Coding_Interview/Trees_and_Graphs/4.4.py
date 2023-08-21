c197f1b5589d835a07052d068e75ab32ee427cf2
MuradAles <muradaleskerov99@gmail.com>
2023-08-21 17:57:29 -0400

    4.4 been added

--- Coding_Interview/Trees_and_Graphs/4.4.py
+++ Coding_Interview/Trees_and_Graphs/4.4.py
@@ -12,6 +12,19 @@ def printTree(node, level=0):
         printTree(node.right, level + 1)
 
 
+def depth(tree):
+    if tree == None:
+        return 0
+    if tree.left == None and tree.right == None:
+        return 1
+    else:
+        depthLeft = 1 + depth(tree.left)
+        depthRight = 1 + depth(tree.right)
+        print("depthLeft", depthLeft)
+        print("depthRight", depthRight)
+        return abs(depthLeft - depthRight) <= 1 or depthLeft == depthRight
+
+
 def main():
     root_1 = TreeNode(50)
     root_1.left = TreeNode(40)
@@ -23,6 +36,7 @@ def main():
     root_1.right.left = TreeNode(65)
     root_1.right.right.right = TreeNode(75)
     printTree(root_1)
+    print(depth(root_1))
     print("-----------------")
     root_2 = TreeNode(50)
     root_2.left = TreeNode(40)
@@ -32,6 +46,7 @@ def main():
     root_2.right.left = TreeNode(65)
     root_2.right.right.right = TreeNode(75)
     printTree(root_2)
+    print(depth(root_2))
 
 
 main()
